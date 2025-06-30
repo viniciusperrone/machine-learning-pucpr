944338a1ca1e83f149bcf92fa194e3eb72ec1523
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-06-29 22:06:23 -0300

    Exploratory analysis

--- main.py
+++ main.py
@@ -15,3 +15,9 @@ warnings.filterwarnings('ignore')
 # Importação do Dataset
 df = pd.read_excel("csgo_round_snapshots.xlsx")
 df.head()
+
+# Análise exploratória
+df.info()
+df.describe()
+df['round_winner'].value_counts().plot(kind='bar')
+plt.title('Distribuição do Vencedor do Round')
